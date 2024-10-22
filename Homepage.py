a19dd13c56a9b3f73e93eafb3c276eb2524a468f
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2024-10-22 13:44:02 +0300

    added new features

--- Homepage.py
+++ Homepage.py
@@ -4,11 +4,10 @@ import joblib
 # nmf modelini yüklüyorum projeme.
 nmf_model = joblib.load('nmf_model.pkl')
 
-st.title('Kategori Analizi')
-st.write('Haber açıklamalarını analiz ederek kategorilere ayırabilirsiniz.')
-
+st.title('Category Analysis')
+st.write("You can categorise news descriptions by analysing them.")
 # Kullanıcıdan description alacağım.
-user_input = st.text_area('Lütfen analiz etmek istediğiniz metni buraya girin:')
+user_input = st.text_area("Please enter the text you want to analyse here:")
 
 tfidf_vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
@@ -22,7 +21,7 @@ categories = [
     "United Kingdom Policy"
 ]
 
-if st.button('Tahmin Et'):
+if st.button('Predict'):
     if user_input:
         # Kullanıcıdan alınan metni vektörleştir
         user_input_tfidf = tfidf_vectorizer.transform([user_input])
@@ -32,6 +31,6 @@ if st.button('Tahmin Et'):
 
         # En yüksek olasılık değerini ve buna karşılık gelen kategoriyi bul
         max_index = topic_distribution[0].argmax()
-        st.write(f"Tahmin Edilen Kategori: {categories[max_index]}")
+        st.write(f"Estimated Category: {categories[max_index]}")
     else:
-        st.warning('Lütfen bir metin girin.')
+        st.warning('Please enter a text.')
